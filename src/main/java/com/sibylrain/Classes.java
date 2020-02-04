69ca7a4cb8616794d323b7806e57d0e565b37d83
zhengwy <zhengwy@epsoft.com.cn>
2020-02-04 14:20:54 +0800

    1

---
+++ src/main/java/com/sibylrain/Classes.java
@@ -0,0 +1,9 @@
+package com.sibylrain;
+
+/**
+ * @Author zhengwy
+ * @Date 2020/2/4 14:19
+ * @Description:
+ */
+public class Classes {
+}
---
+++ src/main/java/com/sibylrain/User.java
@@ -0,0 +1,10 @@
+package com.sibylrain;
+
+/**
+ * @Author zhengwy
+ * @Date 2020/2/4 14:09
+ * @Description:
+ */
+public class User {
+    private String name;
+}
